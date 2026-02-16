ed067c322b1c90fa309dfb3a1eae823f2aa55fcc
Failduck <48665257+Failduck@users.noreply.github.com>
2026-02-15 23:49:03 -0700

    feature/ffmpeg-validation-CLI
    Added FFmpeg validation to CLI (based on approach used in GUI)
    
    Updated Typer args to incorporate Audio bitrate and Video resolution

--- cli.py
+++ cli.py
@@ -1,6 +1,8 @@
 from src.youtube_downloader import YouTubeDownloader
 from src.config import setup_directories
 from typing import Annotated
+import shutil
+import subprocess
 import typer
 
 app = typer.Typer()
@@ -12,12 +14,16 @@ def main(
         typer.Argument(help='\"https://www.youtube.com/watch?v=VIDEO_ID\"')
     ],
     audio_only: Annotated[
-        bool,
-        typer.Option("--audio", "-a", help="Download MP3 Audio only.")
-    ] = False,
+        str,
+        typer.Option("--audio", "-a", help='Download MP3 Audio only.')
+    ] = None,
+    resolution: Annotated[
+        str,
+        typer.Option("--video-res","-v", help='Video Resolution.')
+    ] = None,
     output_dir: Annotated[
         str | None,
-        typer.Option("--output-dir", "-o", help="Copy download to specified folder.")
+        typer.Option("--output-dir", "-o", help='Copy download to specified folder.')
     ] = None,
 ):
 
@@ -26,6 +32,8 @@ def main(
     # Catch Invalid Video URL
     if not url.startswith(('https://www.youtube.com/', 'https://youtu.be/')):
         raise ValueError(f"Error: {url} is not a valid YouTube URL.")
+
+    verify_ffmpeg()
 
     setup_directories()
     
@@ -45,12 +53,30 @@ def main(
     
     print("Starting download...")
     format = 'mp3' if audio_only else 'mp4'
-    success = downloader.download(url, format=format, output_dir=output_dir)
+    
+    success = downloader.download(url, format, resolution, bitrate=audio_only, output_dir=output_dir)
     
     if success:
         print("Download completed!")
     else:
         raise Exception("Error: Download failed.")
+      
+def verify_ffmpeg():
+    ffmpeg_path = shutil.which('ffmpeg')
+    if not ffmpeg_path:
+        raise Exception("Error: FFmpeg is not installed.")
+
+    verified = subprocess.run(
+            [ffmpeg_path, '-version'],
+            capture_output=True,
+            text=True,
+            timeout=3
+        )
+    
+    if not verified:
+        raise Exception("Error: FFmpeg validation failed.")
+    
+    print("FFmpeg Install Verified.")
 
 if __name__ == "__main__":
     app()
